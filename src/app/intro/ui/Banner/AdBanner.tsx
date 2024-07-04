d499cb34fd6a9dcbff25d1b4356bfe7f8794de5c
a-honey <jungahyeon0512@khu.ac.kr>
2024-07-04 20:38:56 +0900

    🔥 Remove: Image/next 사이즈 지정 변경

--- src/app/intro/ui/Banner/AdBanner.tsx
+++ src/app/intro/ui/Banner/AdBanner.tsx
@@ -2,7 +2,11 @@ import Image, { StaticImageData } from "next/image";
 
 const AdBanner = ({ src }: { src: StaticImageData }) => {
   return (
-    <Image src={src} alt="배너 광고 이미지" width={1200} height={186.08} />
+    <Image
+      src={src}
+      alt="배너 광고 이미지"
+      className="absolute top-0 right-0 left-0 bottom-0"
+    />
   );
 };
 export default AdBanner;
